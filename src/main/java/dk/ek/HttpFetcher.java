494bc732a4167a25ae350be78abde867bb259eb5
Thomas Hartmann <thomas@webtrade.dk>
2026-09-08 10:50:33 +0200

    minor

--- src/main/java/dk/ek/HttpFetcher.java
+++ src/main/java/dk/ek/HttpFetcher.java
@@ -9,12 +9,16 @@ import java.net.http.HttpResponse;
 import java.time.Duration;
 
 public class HttpFetcher {
+    private static final String CITY_URL = "https://dawa.aws.dk/steder?hovedtype=Bebyggelse&undertype=by&prim%C3%A6rtnavn=Roskilde";
+
     static void main() {
+        HttpFetcher httpFetcher = new HttpFetcher();
         try(HttpClient client = HttpClient.newBuilder().connectTimeout(Duration.ofSeconds(5)).build()){
+            httpFetcher.fetch(client, CITY_URL);
 
         }
     }
-    public FetchResult fetch(HttpClient client, String url){
+    public void fetch(HttpClient client, String url){
         // 1. Build an HttpRequest for the URL.
         // Create a request
         HttpRequest request = null;
@@ -31,6 +35,8 @@ public class HttpFetcher {
         if (response.statusCode() != 200) {
             throw new RuntimeException("GET request failed. Status code: " + response.statusCode());
         }
+        String body = response.body();
+            System.out.println(body);
         // 2. Add a request timeout.
         // 3. Send the request.
         // 4. Calculate the request duration.
@@ -38,6 +44,5 @@ public class HttpFetcher {
         } catch (URISyntaxException | InterruptedException | IOException e) {
             throw new RuntimeException(e);
         }
-        return null;
     }
 }
